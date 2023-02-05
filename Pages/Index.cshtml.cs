0f7c0670ef2ef99ba857365212d63f5155008add
saw-friendship <Aleksandr.Timoshenko@leroymerlin.ru>
2023-02-05 16:03:26 +0300

    rename project

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebPowerShell.Pages;
+namespace PowerShellWebService.Pages;
 
 public class IndexModel : PageModel
 {
