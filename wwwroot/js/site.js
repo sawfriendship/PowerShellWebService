89b622d0b95ff2102378a6c8144c7a4e7d435f49
saw-friendship <Aleksandr.Timoshenko@leroymerlin.ru>
2023-04-30 17:57:58 +0300

    js prop toggle

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -96,35 +96,47 @@ function ani_send(d) {
 function put_to_body() {
     var result = {};
     $('#Params').children().each(function(){
-        var dict = {}; $(this).find('.param_prop').each(function(i,Prop){dict[Prop.name]=Prop.value})
-        if (dict['Property']) {
-            var Type = dict['Type']
-            var Value = dict['Value'];
-            if (Type == 'String') {
-                v = Value
-            } else if (Type == 'Number') {
-                try {v = Number(Value)} catch (e) {v = Value}
-            } else if (Type == 'Bool') {
-                if (!Value) {v = false} else {try {v = Boolean(JSON.parse(Value.toLowerCase()))} catch (e) {v = Value}}
-            } else if (Type == 'Object') {
-                if (!Value) {v = {}} else {try {v = JSON.parse(Value)} catch (e) {v = Value}}
-            }
+        var dict = {};
+		var toggles = $(this).find('i[class="bi-toggle-on"]')
+		if (toggles.length) {
+			$(this).find('.param_prop').each(function(i,Prop){dict[Prop.name]=Prop.value});
+			if (dict['Property']) {
+				var Type = dict['Type']
+				var Value = dict['Value'];
+				if (Type == 'String') {
+					v = Value
+				} else if (Type == 'Number') {
+					try {v = Number(Value)} catch (e) {v = Value}
+				} else if (Type == 'Bool') {
+					if (!Value) {v = false} else {try {v = Boolean(JSON.parse(Value.toLowerCase()))} catch (e) {v = Value}}
+				} else if (Type == 'Object') {
+					if (!Value) {v = {}} else {try {v = JSON.parse(Value)} catch (e) {v = Value}}
+				}
 
-            result[dict['Property']] = v;
-        }
+				result[dict['Property']] = v;
+			}
+		}
     })
     var j_string = JSON.stringify(result);
     $('#_body').val(j_string)
 }
 
-function add_param() {
-    $('#Param').clone().appendTo('#Params').show(200);
-    $('._btn_del_param').bind('click', del_param)
-} 
+function add_param() {$('#Param').clone().appendTo('#Params').show(200);} 
 
-function del_param() {
-    $(this).parent().hide(200, function(){ $(this).remove(); put_to_body();});
-}
+function del_param(item) {item.parent().hide(200, function(){$(this).remove(); put_to_body();});}
+
+function toggle_param(item) {
+	var property = item.parent().find('input[name=Property]').val();
+	item.find('i').each(function(){
+		if ($(this).hasClass('bi-toggle-on')) {
+			console.log(`property '${property}' is off`);
+			$(this).removeClass('bi-toggle-on').addClass('bi-toggle-off');
+		} else if ($(this).hasClass('bi-toggle-off')) {
+			console.log(`property '${property}' is on`);
+			$(this).removeClass('bi-toggle-off').addClass('bi-toggle-on');
+		} else {}
+	})
+};
 
 function update_url() {
     var url = '/PowerShell'
@@ -193,24 +205,22 @@ function send_body() {
     $.ajax(request);
 }
 
-function write_to_clip() {
-    navigator.clipboard.writeText($('#_result').val())
-}
+function write_to_clip() {navigator.clipboard.writeText($('#_result').val())}
 
 function cache_reload() {$.ajax({url:'/reload',success:function(responce){load_wrapper_form();}})}
 function cache_clear() {$.ajax({url:'/clear',success:function(responce){load_wrapper_form();}})}
 
-$( document ).ready(function() {
+$(document).ready(function() {
     load_wrapper_form();
-
     $('#_wrapper').on('change', function(e){var wrapper = $(this).val(); localStorage['wrapper'] = wrapper; load_script_form(wrapper);});
     $('#_script').on('change', function(e){var script = $(this).val(); localStorage['script'] = script; update_url()});
-    
+	$('#Params').on('click', '._btn_toggle_param', function(){toggle_param($(this))});
+	$('#Params').on('click', '._btn_del_param', function(){del_param($(this))});
     $('#Params').bind('click keyup', put_to_body);
     $('._btn_add_param').bind('click', add_param);
-    $('._btn_del_param').bind('click', del_param);
     $('._btn_send').bind('click', send_body);
     $('._btn_copy').bind('click', write_to_clip);
     $('._btn_reload').bind('click', cache_reload);
     $('._btn_clear').bind('click', cache_clear);
+
 });
