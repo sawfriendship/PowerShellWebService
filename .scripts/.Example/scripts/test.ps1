c0937832972d54b5c8b3413dc227e085af61139d
saw-friendship <Aleksandr.Timoshenko@leroymerlin.ru>
2023-06-02 20:08:30 +0300

    reload scripts

--- .scripts/.Example/scripts/test.ps1
+++ .scripts/.Example/scripts/test.ps1
@@ -1,7 +1,3 @@
-# throw 13
-# 1/0
-# Write-Verbose -Verbose 'verb'
-# 1
 [ordered]@{
     StartUp = $StartUp
     UserCredential = $UserCredential
--- .scripts/.Example/wrapper.ps1
+++ .scripts/.Example/wrapper.ps1
@@ -10,16 +10,21 @@ param(
 
 if ($Body) {
 	[hashtable]$Params = ConvertFrom-Json -InputObject $Body -AsHashtable   
+} elseif ($Query.Count) {
+    [hashtable]$Params = $Query
 } else {
     [hashtable]$Params = @{}
 }
 
+$RouteValues = $Context.Request.RouteValues | % -Begin {$h=@{}} -Process {$h[$_.Key]=$_.Value} -End {$h}
+
+$Wrapper = $RouteValues['Wrapper']
+$Script = $RouteValues['Script']
+
+Start-Transcript -Path "$PSScriptRoot\Transcript\$Script\$((Get-Date).ToString('yyyy-MM-dd'))\$((Get-Date).ToString('yyyy-MM-dd_HH-mm-ss-ffffff')).txt" -Force | Out-Null
+
 # Using global var, that configured in CachedVariables section of config.json file
 if (!$Global:StartUp) {$Global:StartUp = Get-Date}
-
-$ScriptItem = Get-Item -Path $ScriptFile
-$ScriptName = $ScriptItem.BaseName
-Start-Transcript -Path "$PSScriptRoot\Transcript\$ScriptName\$((Get-Date).ToString('yyyy-MM-dd'))\$((Get-Date).ToString('yyyy-MM-dd_HH-mm-ss-ffffff')).txt" -Force | Out-Null
 
 Set-Alias -Name 'Script' -Value $ScriptFile
 
